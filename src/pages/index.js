98ef20716b396304a2b7d878447e61562703203d
kntjhnllc <kentjohnliloc@gmail.com>
2023-10-28 19:35:03 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className='flex text-3xl items-center justify-center w-full h-screen'>
+      <h1>Welcome to my first commit for SASO Attendance Monitoring System</h1>
+    </div>
+  );
+};
+
+export default HomePage;
