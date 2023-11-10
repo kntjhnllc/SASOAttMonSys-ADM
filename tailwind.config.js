4e5e75db01017ec052223157f42dc190d3257c4c
kntjhnllc <kentjohnliloc@gmail.com>
2023-11-10 12:57:17 +0800

    font-monserrat all

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif'],
+
         'lato': ['Lato'],
         'garamond': ['Garamond']
       },
